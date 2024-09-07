6e971076661782b298c97cc4b2d76c14e427da34
Jgrporto26 <134015794+Shoshiki@users.noreply.github.com>
2024-09-06 22:50:07 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil.",
+      link: "https://g.co/kgs/CmdJeYS",
+      tags: "skate fada sk8"
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro em sua categoria. Sua técnica e determinação a colocaram como uma das principais atletas do judô mundial.",
+      link: "https://g.co/kgs/CpVz3Lu" ,
+      tags: "judo judô"
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta brasileira que encantou o mundo com suas acrobacias e força. Nas Olimpíadas de Paris 2024, ela conquistou diversas medalhas, consolidando seu lugar como uma das maiores ginastas da história do Brasil.",
+      link: "https://g.co/kgs/5xj9u1X" ,
+      tags: "ginasta ginasitca"
+    }
+  ];
